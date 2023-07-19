cf7ba52a3e0f968c8e63633fcbc513c09dedf414
Molly Starnes <mstrnes2@gmail.com>
2023-07-19 13:44:05 -0500

    added code to java to generate password

--- Assets/script.js
+++ Assets/script.js
@@ -2,23 +2,52 @@
 var generateBtn = document.querySelector("#generate");
 var specialCharactersArray = ["!","@","#","$","%","^","&","*","(",")","-"];
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var lowserCase =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var lowerCase =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var numbers = ["1","2","3","4","5","6","7","8","9","0"];
 
 function generatePassword(){
-  var characterLength = prompt("How many characters does your password need to contain?");
-  if(characterLength > 8 && characterLength < 128){
-    var caseSensative = confirm("Is your password case sensative?");
-    if(caseSensative){
-      var specialCharacters = confirm("Does this password need any special characters?");
-    }else
+  var passwordChoice = [];
+  var pickedPassword = "";
+  var characterLength = parseInt(prompt("How many characters does your password need to contain?"));
+  var toUpperCase = confirm("Does your password need to contain upper case letters?");
+  var toLowerCase = confirm("Does your password need to contain lower case letters?");
+  var toSpecialCharacter = confirm("Does your password need to contain special characters? (Ex: !@#$)");
+  var toNumbers = confirm("Does your password need to contain numbers?");
 
-  }else{
-    alert("Password has to be between 8 - 128 characters long. Please try again!");
+
+  if(isNaN(characterLength) || characterLength < 8 || characterLength > 128){
+    alert("Password has to be a number between 8 - 128 characters long. Please try again!");
     return;
   }
 
-  return;
+  
+  if(toUpperCase){
+    passwordChoice = passwordChoice.concat(upperCase);
+  }
+
+  
+  if(toLowerCase){
+    passwordChoice = passwordChoice.concat(lowerCase);
+  }
+
+  
+  if(toSpecialCharacter){
+    passwordChoice = passwordChoice.concat(specialCharactersArray);
+  }
+
+  
+  if(toNumbers){
+    passwordChoice = passwordChoice.concat(numbers);
+  } 
+  
+
+  for(var i = 0; i < characterLength; i++){
+    var getPassword = Math.floor(Math.random() * passwordChoice.length);
+    pickedPassword = pickedPassword + passwordChoice[getPassword];
+  }
+  
+  return pickedPassword;
+
 }
 
 // Write password to the #password input
--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # password-generator
+
+## Description
+
+In this project I am using javascript to write a password generator. I give the user prompts to answer to see how many characters they need to use, if the would like upper or lowercase letters, if they would like any special characters and if they would like to use any numbers in the password. After the prompts are answered, the user is given a random password based on the prompts answered.
+
+## Application
+
+Screenshot of application:
+
+![alt text](assets/Screenshot (6).png)
+
+
+Application link: 
+
+## Usage
+
+This website can be used to see my work as a coder using HTML/CSS. You can contact me on the bottom of the page and also click on the GitHub and project links to see my projects first hand. 
+
+
+```md
+![alt text](assets/images/Screenshot%20(2).png)
+```
+```md
+![alt text](assets/images/Screenshot%20(3).png)
+```
+```md
+![alt text](assets/images/Screenshot%20(4).png)
+```
+
+## Credits
+
+https://www.w3docs.com
+
+https://stackoverflow.com
+
+https://youtube.com
+
+https://freecodecamp.com
+
+https://github.com
